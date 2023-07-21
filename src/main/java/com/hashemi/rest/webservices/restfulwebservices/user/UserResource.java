2e2aac0108d8fd15de8733c6bd132168695814c4
mohammad hashemi <mohammad.hashemi747@gmail.com>
2023-07-21 14:26:55 +0330

    add user resource to get all users

---
+++ src/main/java/com/hashemi/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,21 @@
+package com.hashemi.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private UserDaoService userDaoService;
+
+    public UserResource(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+}

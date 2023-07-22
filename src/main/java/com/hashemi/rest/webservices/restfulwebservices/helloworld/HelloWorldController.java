44dc2662db6317aaaf9330f9ccdd72649d573585
Hashemi.m <hashemi.m@tiddev.com>
2023-07-22 14:06:48 +0330

    Internationalization add the second part of content negotiation, making use of accept headers to either send an english response or any other response by (headers: accept-language -> nl for Netherlands)

--- src/main/java/com/hashemi/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/hashemi/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.hashemi.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -21,4 +31,17 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean("Hello World " + name);
     }
+
+    @GetMapping(path = "/hello-world-internationalization")
+    public String helloWorldInternationalization() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(
+                "good.morning.message",
+                null,
+                "Default Message",
+                locale);
+//        return "Hello World";
+
+    }
+
 }

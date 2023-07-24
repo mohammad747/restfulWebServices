d64f780e7fb873f3341474f81bc1136d659a874a
Hashemi.m <hashemi.m@tiddev.com>
2023-07-24 11:04:31 +0330

    Customizing Rest API Responses Return only selected fields Example : filter out passwords Two Types: - static filtering : @JsonIgnoreProperties @JsonIgnore - Dynamic filtering : @JsonFilter with FilterProvider

--- src/main/java/com/hashemi/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/hashemi/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,12 +1,14 @@
 package com.hashemi.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties("field1")
+//@JsonIgnoreProperties("field1")
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
-    @JsonIgnore
+//    @JsonIgnore
     private String field2;
     private String field3;
 

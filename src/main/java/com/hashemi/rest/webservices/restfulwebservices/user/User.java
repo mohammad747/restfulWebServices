b82138cf069c882800ff61f6faca96ddb24a67ff
mohammad hashemi <mohammad.hashemi747@gmail.com>
2023-07-26 18:03:07 +0330

    Post entity and many-to-one relationship

--- src/main/java/com/hashemi/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/hashemi/rest/webservices/restfulwebservices/user/User.java
@@ -1,15 +1,19 @@
 package com.hashemi.rest.webservices.restfulwebservices.user;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
+
 @Entity(name = "user_details")
 public class User {
     @Id
@@ -21,6 +25,10 @@ public class User {
     @Past(message = "birthDate should be in the past")
     @JsonProperty("birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 insert into user_details (birth_date, id, name) values (CURRENT_DATE(), 10001, 'mohammad');
 insert into user_details (birth_date, id, name) values (CURRENT_DATE(), 10002, 'Hossein');
 insert into user_details (birth_date, id, name) values (CURRENT_DATE(), 10003, 'Ali');
+
+
+insert into post (id, user_id, description) values (20001, 10001, 'I want to learn AWS');
+insert into post (id, user_id, description) values (20002, 10002, 'I want to learn DevOps');
+insert into post (id, user_id, description) values (20003, 10002, 'I want to learn mechanic');
+insert into post (id, user_id, description) values (20004, 10003, 'I want to learn woodworking');

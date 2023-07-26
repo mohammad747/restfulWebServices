c16ffaae995f9ecd6fba237853ba64c2cfe49f76
mohammad hashemi <mohammad.hashemi747@gmail.com>
2023-07-26 10:51:28 +0330

    Connecting Rest API to H2 database and Creating user entity and some test data

--- src/main/java/com/hashemi/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/hashemi/rest/webservices/restfulwebservices/user/User.java
@@ -2,13 +2,18 @@ package com.hashemi.rest.webservices.restfulwebservices.user;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
-
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "name should have at least two characters")
     @JsonProperty("user_name")
@@ -21,6 +26,10 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details (birth_date, id, name) values (CURRENT_DATE(), 10001, 'mohammad');
+insert into user_details (birth_date, id, name) values (CURRENT_DATE(), 10002, 'Hossein');
+insert into user_details (birth_date, id, name) values (CURRENT_DATE(), 10003, 'Ali');
